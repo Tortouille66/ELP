4179da9343a0e47e983a44d64def15e98e6c52a7
EAlain88 <enzo.l88200@gmail.com>
2026-01-13 17:58:06 +0100

    V2 normalement version prête au test

--- Go/imageproc/image.go
+++ Go/imageproc/image.go
@@ -11,3 +11,27 @@ type Image struct {
     Height int
     Pixels [][]int
 }
+
+func ReceiveImage(reader *bufio.Reader) Image {
+    var w, h int
+    fmt.Fscan(reader, &w, &h)
+
+    pixels := make([][]int, h)
+    for i := 0; i < h; i++ {
+        pixels[i] = make([]int, w)
+        for j := 0; j < w; j++ {
+            fmt.Fscan(reader, &pixels[i][j])
+        }
+    }
+    return Image{w, h, pixels}
+}
+
+func SendImage(conn net.Conn, img Image) {
+    fmt.Fprintf(conn, "%d %d\n", img.Width, img.Height)
+    for i := 0; i < img.Height; i++ {
+        for j := 0; j < img.Width; j++ {
+            fmt.Fprintf(conn, "%d ", img.Pixels[i][j])
+        }
+        fmt.Fprintln(conn)
+    }
+}
--- Go/imageproc/sobel.go
+++ Go/imageproc/sobel.go
@@ -1,5 +1,33 @@
+package imageproc
+
+import "math"
+
 var SobelX = [][]int{
     {-1, 0, 1},
     {-2, 0, 2},
     {-1, 0, 1},
+}
+
+var SobelY = [][]int{
+    {-1, -2, -1},
+    {0, 0, 0},
+    {1, 2, 1},
+}
+
+func ApplySobel(img Image) Image {
+    gx := Convolve(img, SobelX, 8)
+    gy := Convolve(img, SobelY, 8)
+
+    out := make([][]int, img.Height)
+    for i := range out {
+        out[i] = make([]int, img.Width)
+        for j := 0; j < img.Width; j++ {
+            v := int(math.Sqrt(float64(
+                gx.Pixels[i][j]*gx.Pixels[i][j] +
+                gy.Pixels[i][j]*gy.Pixels[i][j],
+            )))
+            out[i][j] = clamp(v)
+        }
+    }
+    return Image{img.Width, img.Height, out}
 }
